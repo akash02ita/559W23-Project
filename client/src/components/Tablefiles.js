009d27a1a30be0b0ef294215ad9c0073a6962420
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-02-23 18:47:41 -0700

    New Table Headers & Trash icon
    Signed-off-by: CarlosVeinti <carlosveintimillam@hotmail.com>

--- client/src/components/Tablefiles.js
+++ client/src/components/Tablefiles.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import {RiDeleteBin6Line} from 'react-icons/ri'
 
 // Used for "Download Button"
 const Button = styled.button`
   background-color: #0072C6;
   color: white;
-  padding: 5px 15px;
+  padding: 5px 5px ;
   border-radius: 10px;
   border-color: #FFFFFF;
   outline: 0;
@@ -19,7 +20,11 @@ const Button = styled.button`
 
 function downloadFn(){
     alert('Download Button was clicked');
-  }
+}
+
+function deleteFile(){
+    alert('Delete Button was clicked');
+}
 
 function Tablefiles(){
     return(
@@ -27,29 +32,45 @@ function Tablefiles(){
         <table>
           <thead>
             <tr>
+              <th>Select</th>  
               <th>Type</th>
               <th>Name</th>
               <th>Last Modified</th>
+              <th>Last Modified by</th>
               <th>File Size</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             <tr>
+                <td></td>
                 <td>Excel</td>
                 <td>Q1 Balance Sheet.xlsx</td>
                 <td>31/01/2023</td>
+                <td>Akashdeep Singh</td>
                 <td>4 MB</td>
-                <td><Button onClick={downloadFn} className="Download_Btn"> Download </Button></td>
+                <td>
+                    <div className='actionItems'>
+                        <Button onClick={downloadFn} className="Download_Btn"> Download </Button>
+                        <RiDeleteBin6Line size={30} onClick={deleteFile}/>
+                    </div>
+                </td>
 
             </tr>
 
             <tr>
+                <td></td>
                 <td>PDF</td>
                 <td>Stakeholder Relations.pdf</td>
                 <td>20/01/2022</td>
+                <td>Carlos Veintimilla</td>
                 <td>150 MB</td>
-                <td><Button onClick={downloadFn} className="Download_Btn"> Download </Button></td>
+                <td>
+                    <div className='actionItems'>
+                        <Button onClick={downloadFn} className="Download_Btn"> Download </Button>
+                        <RiDeleteBin6Line size={30} onClick={deleteFile}/>
+                    </div>
+                </td>
 
             </tr>
           </tbody>
