e2d4c3d3b8b4d338ade21e9053482722973cd261
harsweet <64262868+harsweet@users.noreply.github.com>
2023-02-25 13:25:37 -0700

    Adds support for saving the uploaded files

--- server/main.py
+++ server/main.py
@@ -1,8 +1,16 @@
+import shutil
 from fastapi import FastAPI, File, UploadFile
 
 app = FastAPI()
 
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+async def create_upload_file(uploaded_file: UploadFile):
+    file_location = f"/Users/harsh/Documents/UploadedFiles/{uploaded_file.filename}"
+
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(uploaded_file.file, file_object)
+
+    return {
+        "info": f"The file '{uploaded_file.filename}' has been successfully uploaded"
+    }
