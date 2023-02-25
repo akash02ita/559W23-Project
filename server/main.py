cf39d9464058792fddff0446fe0b918e7c1958f8
harsweet <64262868+harsweet@users.noreply.github.com>
2023-02-25 13:43:37 -0700

    Adds support for downloading a file

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 import shutil
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
@@ -15,6 +16,15 @@ async def create_upload_file(uploaded_file: UploadFile):
     return {
         "info": f"The file '{uploaded_file.filename}' has been successfully uploaded"
     }
+
+
+@app.get("/downloadfile/")
+async def download_uploaded_file(file_name: str):
+    file_location = f"/Users/harsh/Documents/UploadedFiles/{file_name}"
+
+    return FileResponse(
+        path=file_location, filename=file_name, media_type="application/octet-stream"
+    )
 
 
 if __name__ == "__main__":
