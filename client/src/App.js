4c656920f9224f0814abf2bc44a65a82668cfa74
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-02-27 14:29:34 -0700

    Combined server and client for upload communication

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,28 @@ import './App.css';
 import styled from 'styled-components';
 import { BsUpload } from 'react-icons/bs'
 import Tablefiles from "./components/Tablefiles.js"
+import axios from 'axios';
+import { useState } from 'react';
 
 
-
-function uploadFn(){
-  alert('Upload Button was clicked');
-}
 
 function App() {
+  const [upfile, setUpfile] = useState(null);
+  function uploadFn(){
+    if (!upfile) return;
+
+    console.log("What is upfile?", upfile)
+    const formData = new FormData();
+    formData.append("uploaded_file", upfile);
+    axios.post("/uploadfile", formData, {headers :{
+      "Content-Type": "multipart/form-data",
+    }})
+    .then(res => {
+      console.log(res);
+      console.log(res.data);
+    })
+    //alert('Upload Button was clicked');
+  }
   return (
     <>
       <div className="App">
@@ -27,12 +41,18 @@ function App() {
       <div className='ButtonRow'>
         <div>
           <main>
-            <form onClick={ () => document.querySelector(".input-field").click()}>
-              <input type="file" accept='image/*' className='input-field' hidden />
+            <div onClick={ () => document.querySelector(".input-field").click()}>
+              <input type="file" accept='image/*' className='input-field' hidden onChange={(e) => setUpfile(e.target.files[0])}/>
+              <button onClick={uploadFn}>Upload</button>
 
               <BsUpload size={36} className='uploadButton'/>
 
-            </form>
+            </div>
+
+<form action="/uploadfile/" encType="multipart/form-data" method="post">
+<input name="uploaded_file" type="file" multiple />
+<input type="submit" />
+</form>
           
           </main>
         </div>
--- server/main.py
+++ server/main.py
@@ -1,18 +1,43 @@
-import os
-import shutil
+import shutil # For high level operations on files and collections
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 
+ 
+
+credentials = True
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
+#adding cors urls
+origins = [
+    'http://localhost:3000'
+]
+
+# adding middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = credentials,
+    allow_methods = ["*"],
+    allow_headers = ["*"],
+)
+
+
+@app.post("/helloClient/")
+async def testCommunication():
+    print("Testing message in Server")
+    return {
+        "info": f"A client has communicated with server"
+    }
 
 @app.post("/uploadfile/")
 async def create_upload_file(uploaded_file: UploadFile):
-    file_location = f"/Users/harsh/Documents/UploadedFiles/{uploaded_file.filename}"
+    print("Hello!!!!!!!!")
+    file_location = f"/Users/carlosveint/Documents/UploadedFiles559/{uploaded_file.filename}"
 
     with open(file_location, "wb+") as file_object:
-        shutil.copyfileobj(uploaded_file.file, file_object)
+        shutil.copyfileobj(uploaded_file.file, file_object) # first argument is the source, to be copied to second variable (the destination).
 
     return {
         "info": f"The file '{uploaded_file.filename}' has been successfully uploaded"
@@ -21,10 +46,10 @@ async def create_upload_file(uploaded_file: UploadFile):
 
 @app.get("/downloadfile/")
 async def download_uploaded_file(file_name: str):
-    file_location = f"/Users/harsh/Documents/UploadedFiles/{file_name}"
+    file_location = f"/Users/carlosveint/Documents/UploadedFiles559/{file_name}"
 
     return FileResponse(
-        path=file_location, filename=file_name, media_type="application/octet-stream"
+        path=file_location, filename=file_name, media_type="application/octet-stream" #To indicate that a body contains arbitrary binary data.
     )
 
 
