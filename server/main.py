f9b8710e7266bd423f0c1ce1a31a7b5829cebd8a
harsweet <64262868+harsweet@users.noreply.github.com>
2023-03-05 17:56:49 -0700

    Updates rpyc client-server approach with central node - replica

--- server/main.py
+++ server/main.py
@@ -1,22 +1,28 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import StreamingResponse
-from rpc.rpc_client import *
+import rpyc
 
 app = FastAPI()
-my_client = RPCClient()
+
+# We can change this afterwards when deploying
+central_node_ip = "localhost"
+central_node_port = 8000
+central_node_conn = rpyc.connect(central_node_ip, central_node_port)
 
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
     stuff_read = await file.read()
-    response = my_client.upload(filename=file.filename, data=stuff_read)
+    response = central_node_conn.root.upload_file(
+        filename=file.filename, data=stuff_read
+    )
     return {"info": response.decode()}
 
 
 @app.get("/downloadfile/{filename}")
 async def download_uploaded_file(filename: str):
-    file_data = my_client.download(filename)
+    file_data = central_node_conn.root.download_file(filename)
     return StreamingResponse(
         iter([file_data]),
         headers={
@@ -28,7 +34,7 @@ async def download_uploaded_file(filename: str):
 
 @app.get("/fileslist/")
 async def get_list_of_files():
-    files_array = my_client.get_list()
+    files_array = central_node_conn.root.get_list()
     return {"files": files_array}
 
 
