a2d3bffbcb2fbe315b8bae6d57135022e0dcce3f
Akashdeep Singh <akash02ita@gmail.com>
2023-03-18 05:08:39 -0600

    reconnect central node every time in main.py when backup node takes over old connection is dead. so a new one must be established. for now reestablishing new connection in every api call

--- server/main.py
+++ server/main.py
@@ -25,21 +25,26 @@ app.add_middleware(
 # We can change this afterwards when deploying
 central_node_ip = "localhost"
 central_node_port = 8000
-central_node_conn = rpyc.connect(central_node_ip, central_node_port)
+# central_node_conn = rpyc.connect(central_node_ip, central_node_port)
 
 
 @app.post("/uploadfile/")
 async def create_upload_file(uploaded_file: UploadFile = File(...)):
     stuff_read = await uploaded_file.read()
+    central_node_conn = rpyc.connect(central_node_ip, central_node_port)
     response = central_node_conn.root.upload_file(
-        filename=uploaded_file.filename, data=stuff_read
+        filename=uploaded_file.filename, 
+        data=stuff_read
     )
+    # central_node_conn.close() # for now no connection closure
     return {"info": response.decode()}
 
 
 @app.get("/downloadfile/{filename}")
 async def download_uploaded_file(filename: str):
+    central_node_conn = rpyc.connect(central_node_ip, central_node_port)
     file_data = central_node_conn.root.download_file(filename)
+    # central_node_conn.close()
     return StreamingResponse(
         iter([file_data]),
         headers={
@@ -51,7 +56,9 @@ async def download_uploaded_file(filename: str):
 
 @app.get("/fileslist/")
 async def get_list_of_files():
+    central_node_conn = rpyc.connect(central_node_ip, central_node_port)
     files_array = central_node_conn.root.get_list()
+    # central_node_conn.close() # must deep copy stuff before closing. for now no connectin closure
     return {"files": files_array}
 
 
