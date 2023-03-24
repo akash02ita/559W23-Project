aaafe3630aac9dfcc948b934b705c1da50e32b60
CarlosVeinti <carlosveintimillam@hotmail.com>
2023-03-23 19:53:20 -0600

    Bootstrap Table & react icons for FE
    New Table using Bootstrap & icons using react-icons version 4.8.0

--- client/src/components/Tablefiles.js
+++ client/src/components/Tablefiles.js
@@ -1,4 +1,7 @@
-import {RiDeleteBin6Line} from 'react-icons/ri'
+import Table from 'react-bootstrap/Table'
+import { BsFiletypeTxt, BsDownload, BsTrash } from 'react-icons/bs'
+import { FiEye } from 'react-icons/fi'
+import { BiCheckbox } from 'react-icons/bi'
 
 
 function downloadFn(){
@@ -13,71 +16,57 @@ function Tablefiles(){
     return(
         <div className='table-container'>
         <h1 className='myFilesHeader'> My Files</h1>            
-        <table>
-          <thead>
-            <tr>
-              <th>Select</th>  
-              <th>Type</th>
-              <th>Name</th>
-              <th>Last Modified</th>
-              <th>Last Modified by</th>
-              <th>File Size</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-                <td></td>
-                <td>Excel</td>
-                <td>Q1 Balance Sheet.xlsx</td>
-                <td>31/01/2023</td>
-                <td>Akashdeep Singh</td>
-                <td>4 MB</td>
-                <td>
-                    <div className='actionItems'>
-                        <button onClick={downloadFn} className="Download_Btn"> Download </button>
-                        <RiDeleteBin6Line size={30} onClick={deleteFile} className='trashIcon'/>
-                    </div>
-                </td>
-
-            </tr>
-
-            <tr>
-                <td></td>
-                <td>PDF</td>
-                <td>Stakeholder Relations.pdf</td>
-                <td>20/02/2022</td>
-                <td>Carlos Veintimilla</td>
-                <td>150 MB</td>
-                <td>
-                    <div className='actionItems'>
-                        <button onClick={downloadFn} className="Download_Btn"> Download </button>
-                        <RiDeleteBin6Line size={30} onClick={deleteFile} className='trashIcon'/>
-                    </div>
-                </td>
-
-            </tr>
-
-            <tr>
-                <td></td>
-                <td>Word</td>
-                <td>SiteA-WeeklyMeetings-Notes.docx</td>
-                <td>15/03/2021</td>
-                <td>Harsweet Singh</td>
-                <td>32 MB</td>
-                <td>
-                    <div className='actionItems'>
-                        <button onClick={downloadFn} className="Download_Btn"> Download </button>
-                        <RiDeleteBin6Line size={30} onClick={deleteFile} className='trashIcon'/>
-                    </div>
-                </td>
-
-            </tr>
-
-
-          </tbody>
-
-        </table>
+        <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Select</th>
+          <th>Type</th>
+          <th>Name</th>
+          <th>Last Modified</th>
+          <th>Last Modified By</th>
+          <th>Size</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td> <BiCheckbox></BiCheckbox></td>
+          <td> <BsFiletypeTxt/> </td>
+          <td>Some File</td>
+          <td>2023-03-23 5:30 PM</td>
+          <td>Steve Jobs</td>
+          <td>3.7 MB</td>
+          <td> <FiEye></FiEye> 
+               <BsDownload></BsDownload>  
+               <BsTrash></BsTrash> 
+          </td>
+        </tr>
+        <tr>
+        <td> <BiCheckbox></BiCheckbox></td>
+          <td> <BsFiletypeTxt/> </td>
+          <td>File Xbox</td>
+          <td>2023-03-23 5:30 PM</td>
+          <td>Bill Gates</td>
+          <td>3.7 MB</td>
+          <td> <FiEye></FiEye> 
+               <BsDownload></BsDownload>  
+               <BsTrash></BsTrash>
+          </td> 
+        </tr>
+        <tr>
+        <td> <BiCheckbox></BiCheckbox></td>
+          <td> <BsFiletypeTxt/> </td>
+          <td>File Azure</td>
+          <td>2023-03-23 5:30 PM</td>
+          <td>Satya Nadella</td>
+          <td>3.7 MB</td>
+          <td> <FiEye></FiEye> 
+               <BsDownload></BsDownload>  
+               <BsTrash></BsTrash>
+          </td>  
+        </tr>
+      </tbody>
+    </Table>
       </div>
     );
 }
