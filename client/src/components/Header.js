63a9c42de14c3eb9ecb02c49df4e2acd1874f89b
harsweet <64262868+harsweet@users.noreply.github.com>
2023-04-10 22:55:29 -0600

    Updates app name, adds app logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import { Navbar, FormControl, InputGroup } from 'react-bootstrap'
+import { Navbar } from 'react-bootstrap'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Logo from '../logo.png'
 
 function Header() {
   return (
     <Navbar bg="navBarcolor" expand="lg" className='app-navbar'>
       <Navbar.Brand href="#home">
-        <h1>DFS-D</h1>
-        <h4>Distributed File System - Drive</h4>
+        <Container>
+          <Row className="justify-content-md-center">
+            <Col style={{alignSelf: "center"}}>
+              <img
+                alt="Logo"
+                src={Logo}
+                style={{width: '4rem'}}
+              />
+              </Col>
+            <Col>
+              <h1>DFS-D</h1>
+              <h4>Distributed File System - Drive</h4>
+            </Col>
+          </Row>
+        </Container>
       </Navbar.Brand>
     </Navbar>
   )
